6486379f977cc9d74ace3e770de3f4c969672659
Publikwerker <hello@publikwerks.com>
2019-09-09 13:04:08 -0700

    moved background image from about to footer, including clip-path. started implementing grid into about section. added some copy and a button

--- OpenJobs/walterCosmopolis/walter_cosmopolis/src/About.js
+++ OpenJobs/walterCosmopolis/walter_cosmopolis/src/About.js
@@ -5,6 +5,17 @@ export default function About() {
     <main>
       <h2 className="about__title">Walter Cosmopolis is world-famous for good reason!</h2>
       <section className="about">
+        <div className="row">
+          <div className="col-1-of-2">
+            <h3 className="heading-tertiary">Walter Fights for Truth and Justice</h3>
+            <p className="paragraph">In his youth, Walter saw injustice in the world. Realizing that he was the one he was waiting for, young Walter put on his boots and went on a Mission.</p>
+
+            <h3 className="heading-tertiary">Walter Gives Voice to the Voiceless</h3>
+            <p className="paragraph">Walter has spent countless hours spreading the word that every person is a star.</p>
+
+            <a href="#" className="btn-text">Learn More &rarr;</a>
+          </div>
+        </div>
       </section>
     </main>
   )
